c479d5fa89547917cb24154fa8e82c95b0fcc515
Josh Fletcher <jfletcher@nuxeo.com>
2018-09-27 17:25:26 -0700

    Working on test scaffolding.

--- nuxeo-dam-solution-core/src/main/java/nuxeo/labs/dam/solution/core/enricher/ThumbnailsEnricher.java
+++ nuxeo-dam-solution-core/src/main/java/nuxeo/labs/dam/solution/core/enricher/ThumbnailsEnricher.java
@@ -1,15 +1,15 @@
 package nuxeo.labs.dam.solution.core.enricher;
 
-import static org.nuxeo.ecm.core.io.registry.reflect.Instantiations.SINGLETON;
-import static org.nuxeo.ecm.core.io.registry.reflect.Priorities.REFERENCE;
-
-import java.io.IOException;
-import java.util.Collections;
-
 import org.codehaus.jackson.JsonGenerator;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.io.marshallers.json.enrichers.AbstractJsonEnricher;
 import org.nuxeo.ecm.core.io.registry.reflect.Setup;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import static org.nuxeo.ecm.core.io.registry.reflect.Instantiations.SINGLETON;
+import static org.nuxeo.ecm.core.io.registry.reflect.Priorities.REFERENCE;
 
 /**
  * Enrich {@link nuxeo.ecm.core.api.DocumentModel} Json.
@@ -38,6 +38,29 @@ public class ThumbnailsEnricher extends AbstractJsonEnricher<DocumentModel> {
 
   @Override
   public void write(JsonGenerator jg, DocumentModel obj) throws IOException {
+
+    /* Here's the old Automation Script:
+
+    function run(input, params) {
+      var deliverable = Document.GetParent(input, {'type': 'Deliverable' });
+      if (typeof deliverable === "undefined" || deliverable===null) {
+        return input;
+      }
+      var thumbnails = [];
+      //get all assets
+      var assets = Document.GetChildren(deliverable, {});
+      assets.forEach(function(asset) {
+        if (asset['picture:views'] ) {
+              thumbnails.push(asset['picture:views'][0].content);
+        } else if (asset['thumb:thumbnail']) {
+               thumbnails.push(asset['thumb:thumbnail']);
+        }
+      });
+      deliverable['deliverable:assets_thumbnails'] = thumbnails;
+      Document.Save(deliverable, {});
+    }
+    */
+
     // How to instanciate a Session if `obj` is a DocumentModel
     //try (SessionWrapper wrapper = ctx.getSession(obj)) {
     //    CoreSession session = wrapper.getSession();
--- nuxeo-dam-solution-core/src/test/java/nuxeo/labs/dam/solution/core/ThumbnailsEnricherTest.java
+++ nuxeo-dam-solution-core/src/test/java/nuxeo/labs/dam/solution/core/ThumbnailsEnricherTest.java
@@ -1,25 +1,29 @@
 package nuxeo.labs.dam.solution.core;
 
-import javax.inject.Inject;
-
 import nuxeo.labs.dam.solution.core.enricher.ThumbnailsEnricher;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.CoreSession;
-import org.nuxeo.ecm.core.api.PathRef;
-import org.nuxeo.ecm.platform.test.PlatformFeature;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.io.marshallers.json.document.DocumentModelJsonWriter;
+import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
 import org.nuxeo.ecm.core.io.marshallers.json.AbstractJsonWriterTest;
 import org.nuxeo.ecm.core.io.marshallers.json.JsonAssert;
+import org.nuxeo.ecm.core.io.marshallers.json.document.DocumentModelJsonWriter;
 import org.nuxeo.ecm.core.io.registry.context.RenderingContext.CtxBuilder;
-import org.nuxeo.runtime.test.runner.Deploy;
-import org.nuxeo.runtime.test.runner.Features;
-import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.ecm.platform.test.PlatformFeature;
+import org.nuxeo.runtime.test.runner.*;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.io.Serializable;
 
 @RunWith(FeaturesRunner.class)
 @Features({PlatformFeature.class})
 @Deploy({"nuxeo.labs.dam.solution.core.nuxeo-dam-solution-core"})
+@PartialDeploy(bundle = "studio.extensions.dam-solution-nuxeo", extensions = {TargetExtensions.ContentModel.class})
+
 public class ThumbnailsEnricherTest extends AbstractJsonWriterTest.Local<DocumentModelJsonWriter, DocumentModel> {
 
   public ThumbnailsEnricherTest() {
@@ -31,6 +35,49 @@ public class ThumbnailsEnricherTest extends AbstractJsonWriterTest.Local<Documen
 
   @Test
   public void test() throws Exception {
+
+    /* Here's the old Automation Script:
+
+      function run(input, params) {
+        var deliverable = Document.GetParent(input, {'type': 'Deliverable' });
+        if (typeof deliverable === "undefined" || deliverable===null) {
+          return input;
+        }
+        var thumbnails = [];
+        //get all assets
+        var assets = Document.GetChildren(deliverable, {});
+        assets.forEach(function(asset) {
+          if (asset['picture:views'] ) {
+                thumbnails.push(asset['picture:views'][0].content);
+          } else if (asset['thumb:thumbnail']) {
+                 thumbnails.push(asset['thumb:thumbnail']);
+          }
+        });
+        deliverable['deliverable:assets_thumbnails'] = thumbnails;
+        Document.Save(deliverable, {});
+      }
+     */
+
+    // Create a Deliverable
+    DocumentModel theDeliverable = session.createDocumentModel(session.getRootDocument().getPathAsString(), "theDeliverable", "Deliverable");
+    theDeliverable.setPropertyValue("deliverable:assignee", "Administrator");
+    session.createDocument(theDeliverable);
+
+    // Create two Picture documents inside the Deliverable
+    DocumentModel pic1 = session.createDocumentModel(theDeliverable.getPathAsString(), "pic1", "Picture");
+    File file1 = new File(getClass().getResource("/files/jpg.jpg").getPath());
+    Blob blob1 = new FileBlob(file1);
+    pic1.setPropertyValue("file:content", (Serializable) blob1);
+    pic1 = session.createDocument(pic1);
+
+    DocumentModel pic2 = session.createDocumentModel(theDeliverable.getPathAsString(), "pic2", "Picture");
+    File file2 = new File(getClass().getResource("/files/png.png").getPath());
+    Blob blob2 = new FileBlob(file2);
+    pic2.setPropertyValue("file:content", (Serializable) blob2);
+    pic2 = session.createDocument(pic2);
+
+    // How do we wait until the thumbnail is created? Or is it synchronous?
+    // Test the enricher
     DocumentModel obj = session.getDocument(new PathRef("/"));
     JsonAssert json = jsonAssert(obj, CtxBuilder.enrich("document", ThumbnailsEnricher.NAME).get());
     json = json.has("contextParameters").isObject();
