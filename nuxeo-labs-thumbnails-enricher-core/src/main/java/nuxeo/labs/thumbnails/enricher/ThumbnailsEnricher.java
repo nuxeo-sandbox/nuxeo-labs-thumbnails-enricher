b4b06799eb115993e20ae9174e793ec6d5c138bf
Josh Fletcher <jfletcher@nuxeo.com>
2018-10-03 08:31:46 -0700

    Implement custom enricher

--- nuxeo-labs-thumbnails-enricher-core/src/main/java/nuxeo/labs/thumbnails/enricher/ThumbnailsEnricher.java
+++ nuxeo-labs-thumbnails-enricher-core/src/main/java/nuxeo/labs/thumbnails/enricher/ThumbnailsEnricher.java
@@ -1,7 +1,9 @@
 package nuxeo.labs.thumbnails.enricher;
 
 import org.codehaus.jackson.JsonGenerator;
+import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentModelList;
 import org.nuxeo.ecm.core.io.marshallers.json.enrichers.AbstractJsonEnricher;
 import org.nuxeo.ecm.core.io.registry.reflect.Setup;
 
@@ -21,7 +23,13 @@ import static org.nuxeo.ecm.core.schema.FacetNames.FOLDERISH;
  * {
  *   ...
  *   "contextParameters": {
- *     "thumbnails": { ... }
+ *     "thumbnails": [
+ *       {
+ *         "id": ...
+ *         "thumbnailUrl": ...
+ *       },
+ *       ...
+ *     ]
  *   }
  * }
  * </pre>
@@ -32,6 +40,10 @@ public class ThumbnailsEnricher extends AbstractJsonEnricher<DocumentModel> {
 
   public static final String NAME = "thumbnails";
 
+  public static final String THUMBNAIL_URL_LABEL = "thumbnailUrl";
+
+  public static final String THUMBNAIL_URL_PATTERN = "%s/api/v1/repo/%s/id/%s/@rendition/thumbnail";
+
   public ThumbnailsEnricher() {
     super(NAME);
   }
@@ -39,38 +51,31 @@ public class ThumbnailsEnricher extends AbstractJsonEnricher<DocumentModel> {
   @Override
   public void write(JsonGenerator jg, DocumentModel theFolderish) throws IOException {
 
-    /* Here's the old Automation Script:
-
-    function run(input, params) {
-      var deliverable = Document.GetParent(input, {'type': 'Deliverable' });
-      if (typeof deliverable === "undefined" || deliverable===null) {
-        return input;
-      }
-      var thumbnails = [];
-      //get all assets
-      var assets = Document.GetChildren(deliverable, {});
-      assets.forEach(function(asset) {
-        if (asset['picture:views'] ) {
-              thumbnails.push(asset['picture:views'][0].content);
-        } else if (asset['thumb:thumbnail']) {
-               thumbnails.push(asset['thumb:thumbnail']);
-        }
-      });
-      deliverable['deliverable:assets_thumbnails'] = thumbnails;
-      Document.Save(deliverable, {});
-    }
-    */
     if (theFolderish.hasFacet(FOLDERISH)) {
 
+      CoreSession session = theFolderish.getCoreSession();
+
       // Get children that have a useful thumbnail? Or just all children, at least to start
-      // Use CtxBuilder.enrich("document", "thumbnail") on each child
+      DocumentModelList theChildren = session.getChildren(theFolderish.getRef());
 
       jg.writeFieldName(NAME);
       jg.writeStartArray();
-      jg.writeStartObject();
-      jg.writeStringField("name", (String) theFolderish.getPropertyValue("dc:title"));
-      jg.writeStringField("thumbnailUrl", "http://foo.com/bar");
-      jg.writeEndObject();
+
+      for (int i = 0; i < theChildren.totalSize(); i++) {
+
+        DocumentModel currentChild = theChildren.get(i);
+
+        jg.writeStartObject();
+        jg.writeStringField("id", (String) currentChild.getId());
+        // This is copied from org.nuxeo.ecm.platform.thumbnail.io.ThumbnailJsonEnricher.java because can't figure out
+        // how to use an existing enricher from Java.
+        jg.writeStringField(THUMBNAIL_URL_LABEL,
+            String.format(THUMBNAIL_URL_PATTERN, ctx.getBaseUrl().replaceAll("/$", ""),
+                currentChild.getRepositoryName(), currentChild.getId()));
+        jg.writeEndObject();
+      }
+
+
       jg.writeEndArray();
     }
   }
--- nuxeo-labs-thumbnails-enricher-core/src/test/java/nuxeo/labs/thumbnails/enricher/ThumbnailsEnricherTest.java
+++ nuxeo-labs-thumbnails-enricher-core/src/test/java/nuxeo/labs/thumbnails/enricher/ThumbnailsEnricherTest.java
@@ -2,20 +2,18 @@ package nuxeo.labs.thumbnails.enricher;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
 import org.nuxeo.ecm.core.io.marshallers.json.AbstractJsonWriterTest;
 import org.nuxeo.ecm.core.io.marshallers.json.JsonAssert;
 import org.nuxeo.ecm.core.io.marshallers.json.document.DocumentModelJsonWriter;
 import org.nuxeo.ecm.core.io.registry.context.RenderingContext.CtxBuilder;
 import org.nuxeo.ecm.platform.test.PlatformFeature;
-import org.nuxeo.runtime.test.runner.*;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
 
 import javax.inject.Inject;
-import java.io.File;
-import java.io.Serializable;
 
 @RunWith(FeaturesRunner.class)
 @Features({PlatformFeature.class})
@@ -33,54 +31,32 @@ public class ThumbnailsEnricherTest extends AbstractJsonWriterTest.Local<Documen
   @Test
   public void test() throws Exception {
 
-    /* Here's the old Automation Script:
-
-      function run(input, params) {
-        var deliverable = Document.GetParent(input, {'type': 'Deliverable' });
-        if (typeof deliverable === "undefined" || deliverable===null) {
-          return input;
-        }
-        var thumbnails = [];
-        //get all assets
-        var assets = Document.GetChildren(deliverable, {});
-        assets.forEach(function(asset) {
-          if (asset['picture:views'] ) {
-                thumbnails.push(asset['picture:views'][0].content);
-          } else if (asset['thumb:thumbnail']) {
-                 thumbnails.push(asset['thumb:thumbnail']);
-          }
-        });
-        deliverable['deliverable:assets_thumbnails'] = thumbnails;
-        Document.Save(deliverable, {});
-      }
-     */
-
     // Create a Folder
     DocumentModel theFolder = session.createDocumentModel(session.getRootDocument().getPathAsString(), "theFolder", "Folder");
     session.createDocument(theFolder);
 
     // Create two documents inside the Folder
-    DocumentModel note1 = session.createDocumentModel(theFolder.getPathAsString(), "note1", "Note");
-    note1.setPropertyValue("dc:title", "note1");
-    session.createDocument(note1);
+    DocumentModel child1 = session.createDocumentModel(theFolder.getPathAsString(), "note1", "Note");
+    child1.setPropertyValue("dc:title", "note1");
+    session.createDocument(child1);
 
-    DocumentModel note2 = session.createDocumentModel(theFolder.getPathAsString(), "note2", "Note");
-    note1.setPropertyValue("dc:title", "note2");
-    session.createDocument(note2);
-
-    // How do we wait until the thumbnail is created? Or is it synchronous?
+    DocumentModel child2 = session.createDocumentModel(theFolder.getPathAsString(), "note2", "Note");
+    child2.setPropertyValue("dc:title", "note2");
+    session.createDocument(child2);
 
     // Test the enricher
     JsonAssert json = jsonAssert(theFolder, CtxBuilder.enrich("document", ThumbnailsEnricher.NAME).get());
     json = json.has("contextParameters").isObject();
+
     // Should be an array of two thumbnail objects returned by the enricher.
     json.has(ThumbnailsEnricher.NAME);
-
     JsonAssert thumbnails = json.get(ThumbnailsEnricher.NAME);
     thumbnails.isArray();
+
+    // Should have thumbnail urls
     JsonAssert child1thumbnail = thumbnails.get(0);
-    child1thumbnail.has("thumbnailUrl");
+    child1thumbnail.has(ThumbnailsEnricher.THUMBNAIL_URL_LABEL);
     JsonAssert child2thumbnail = thumbnails.get(1);
-    child2thumbnail.has("thumbnailUrl");
+    child2thumbnail.has(ThumbnailsEnricher.THUMBNAIL_URL_LABEL);
   }
 }
